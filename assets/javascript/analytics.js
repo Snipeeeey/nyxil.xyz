f509d10b64c876efccc961459542f46612ceb58a
Snipeeeey <73399792+Snipeeeey@users.noreply.github.com>
2022-06-25 21:45:54 +0200

    Update analytics.js

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -3,5 +3,5 @@
 s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
 (window,document,'ga','script','https://www.google-analytics.com/analytics.js');
 
-ga('create', 'UA-122247573-1', 'cockygames.ga');
+ga('create', 'UA-122247573-1', 'outbuilt.ooo');
 ga('send', 'pageview');
