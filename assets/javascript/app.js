d866643361e355f8818ce01774f513542fc330b0
Snipeeeey <73399792+Snipeeeey@users.noreply.github.com>
2022-06-25 21:35:38 +0200

    appthingy.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,52 +2,66 @@
 // Credits to expl0it, shellcode.team
 // GitHub: https://github.com/Lumm1t/obnoxious.club
 
-class _app {
-  id = 0;
-  videoElement = null;
-  audioElement = null;
-  musicVolume = 0.12;
-  musicFadeIn = 4000;
-  skippedIntro = false;
-  backgroundToggler = false;
-  shouldIgnoreVideo = false;
-  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'esoterik fanboys', 'hack vs hack team'];
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "legend",
+        "god",
+        "king",
+        "emporer",
+        "monarch",
+        "czar",
+        "majesty",
+        "prince",
+		"daddy"
 
-  titleChanger = (text, delay) => {
-    if (!text) return;
+    ];
 
-    delay = delay || 2000;
+    this.titleChanger = function (text, delay) {
+        text = text ||
+            ['p1rate', 'P1RATE#3976'];
+        delay = delay || 2000;
 
-    let counter = 0;
+        var counter = 0;
 
-    setInterval(() => {
-      if (counter < text.length) document.title = text[counter++];
-      else document.title = text[(counter = 0)];
-    }, delay);
-  };
+        setInterval(function () {
+            if(counter < text.length) 
+                document.title = text[counter++]; 
+            else 
+                document.title = text[counter = 0];
+        }, delay);
+    }
 
-  iconChanger = (urls, delay) => {
-    if (!urls) return;
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
 
-    delay = delay || 2000;
+        delay = delay || 2000;
 
-    let counter = 0;
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
 
-    setInterval(() => {
-      if (counter < urls.length) {
-        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+           ++counter;
+        }, delay);
+    }
+};
 
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = urls[counter];
-
-        document.getElementsByTagName('head')[0].appendChild(link);
-      } else counter = 0;
-
-      ++counter;
-    }, delay);
-  };
-}
-
-const app = new _app();
+var app = new _app();
